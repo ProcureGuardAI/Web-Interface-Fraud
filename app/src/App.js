1a7412580ced9a9a0997c753c1da2c4c6c668940
calvinarieri <ariericalvin@gmail.com>
2024-10-13 21:01:27 +0300

    created signup form

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,8 @@
-import Login from "./pages/Login";
-
 function App() {
 
   return (
     <div className="font-openSans">
-      <Login />
+ 
     </div>
   );
 }
---
+++ app/src/pages/TransactionStatus.js
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import warning from "../assets/warning.png"
+import loading from "../assets/loading.png"
+import success from "../assets/success.png"
+function TransactionStatus(){
+    let status = "success"
+    return(
+        <div className="flex justify-center items-center h-screen bg-gray-50">
+            <div>
+               {status==="error" && <StatusComponent
+                statusIcon={warning}
+                statusMessage="Transaction was not completed"
+                />}
+                {status === "loading" && <StatusComponent
+                statusIcon={loading}
+                statusMessage="Please wait as we initiate the transaction"
+                spinIcon = {true}
+                />}
+                {status === "success" && <StatusComponent
+                statusIcon={success}
+                statusMessage="Your request was successfull. "
+                />}
+            </div>
+        </div>
+    )
+}
+
+export default TransactionStatus;
+
+function StatusComponent({statusIcon, statusMessage, spinIcon=false}){
+    return(
+        <div className="bg-white px-20 py-4 rounded-xl shadow-2xl shadow-green-300">
+            <div className={`flex justify-center items-center `}>
+                <img className={`${spinIcon && "animate-spin"} w-56 h-56 `} src={statusIcon} alt={statusMessage} />
+            </div>
+            <div>
+                <h1 className="font-light text-2xl text-center my-8">{statusMessage}</h1>
+            </div>
+        </div>
+    )
+}
+
+StatusComponent.propTypes ={
+    statusIcon:PropTypes.string.isRequired,
+    statusMessage:PropTypes.string.isRequired,
+    spinIcon:PropTypes.bool.isRequired
+}
